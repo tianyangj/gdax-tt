fbd100e38b2e1ef3fd09e2ee4170ec68a54918f8
Tommy Jiang <tianyangj@gmail.com>
2017-09-28 17:57:07 -0700

    update main

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 const data = window.data;
-const WARNING = 10000;
+const TOKEN_SIZE = 10;
+const QUEUE_SIZE = 10;
 
 let total = 0;
 let time = new Date(data[0].time).getTime();
+let queue = [];
 const result = data.map(trade => {
   const {
     type,
@@ -19,73 +21,76 @@ const result = data.map(trade => {
   }
   return {
     ...subset,
+    price: Number(trade.price),
+    size: Number(trade.size),
     amount: amount,
     x: (new Date(trade.time).getTime() - time) / 1000,
     y: total
   };
 }).reduce((acc, cur) => {
   let currentTime = new Date(cur.time);
-  let currentYear = currentTime.getUTCFullYear();
-  let currentMonth = currentTime.getUTCMonth() + 1;
-  let currentDate = currentTime.getUTCDate();
-  let currentHour = currentTime.getUTCHours();
-  let currentMinute = currentTime.getUTCMinutes();
-  let node = acc.find(x => x.id === getDateTimeId(currentYear, currentMonth, currentDate, currentHour, currentMinute));
+  let node = acc.find(x => x.id === getDateTimeId(currentTime));
   if (node) {
     if (cur.side === 'buy') {
       node.buy++;
-      node.buyVolume += cur.amount;
+      node.buySize += cur.size;
     } else if (cur.side === 'sell') {
       node.sell++;
-      node.sellVolume += cur.amount;
-    }
-    if (Math.abs(node.netVolume) > WARNING) {
-      node.warning = true;
+      node.sellSize += cur.size;
     }
     node.endPrice = cur.price;
-    node.netVolume = node.buyVolume - node.sellVolume;
+    node.netSize = node.buySize - node.sellSize;
     node.trades.push(cur);
   } else {
     let node = {
-      id: getDateTimeId(currentYear, currentMonth, currentDate, currentHour, currentMinute),
+      id: getDateTimeId(currentTime),
       warning: undefined,
-      netVolume: 0,
+      weight: 0,
+      netSize: 0,
       startPrice: cur.price,
       endPrice: cur.price,
       buy: 0,
-      buyVolume: 0,
+      buySize: 0,
       sell: 0,
-      sellVolume: 0,
+      sellSize: 0,
       trades: [cur]
     };
     if (cur.side === 'buy') {
       node.buy = 1;
-      node.buyVolume = cur.amount;
+      node.buySize = cur.size;
     } else if (cur.side === 'sell') {
       node.sell = 1;
-      node.sellVolume = cur.amount;
+      node.sellSize = cur.size;
     }
-    node.netVolume = node.buyVolume - node.sellVolume;
+    node.netSize = node.buySize - node.sellSize;
     acc.push(node);
   }
   return acc;
-}, []);
+}, []).map(trade => {
+  return {
+    ...trade,
+    warning: Math.abs(trade.netSize) >= TOKEN_SIZE ? true : undefined,
+    weight: getQueueSum(trade.netSize)
+  };
+});
 
-function getDateTimeId(year, month, date, hour, minute) {
-  return `${year}-${month}-${date}T${hour}:${minute}Z`;
+function getDateTimeId(datetime) {
+  let year = datetime.getUTCFullYear();
+  let month = datetime.getUTCMonth() + 1;
+  let date = datetime.getUTCDate();
+  let hour = datetime.getUTCHours();
+  let minute = datetime.getUTCMinutes();
+  let second = datetime.getUTCSeconds();
+  return `${year}-${month}-${date}T${hour}:${minute}:${second}Z`;
 }
 
-// let queue = [];
-// function getLastNSum(trade, n) {
-//   n = n | 100;
-//   let price = trade.side === 'sell' ? -Math.abs(Number(trade.price)) : Number(trade.price);
-//   let size = Number(trade.size);
-//   if (queue.length >= n) {
-//     queue.shift();
-//   }
-//   queue.push(price * size);
-//   return queue.reduce((acc, cur) => acc + cur, 0);
-// }
+function getQueueSum(size) {
+  if (queue.length >= QUEUE_SIZE) {
+    queue.shift();
+  }
+  queue.push(size);
+  return queue.reduce((acc, cur) => acc + cur, 0);
+}
 
 console.log('total', total);
 console.log('result', result);
