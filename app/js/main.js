54330d1eb5639ff7ad57c0faa55065035da25ce6
Tommy Jiang <tianyangj@gmail.com>
2017-09-30 22:32:09 -0700

    refactor

--- app/js/main.js
+++ app/js/main.js
@@ -1,141 +1,130 @@
 const data = window.data;
 const QUEUE_SIZE = 10;
+const K = 1.5;
 
 let queue = [];
-const result = data
-  .map(normalizeData)
-  .reduce(groupBySecond, []);
 
-function normalizeData(trade) {
-  return {
-    time: trade.time,
-    side: trade.side,
-    price: Number(trade.price),
-    size: Number(trade.size),
-    amount: Number(trade.price) * Number(trade.size)
-  };
-}
-
-function groupBySecond(groups, trade) {
-  const tradeTime = new Date(trade.time);
-  let group = groups.find(g => g.id === getDateTimeId(trade.time));
-  if (group) {
-    if (trade.side === 'buy') {
-      group.buyTotal++;
-      group.buySize += trade.size;
-    } else if (trade.side === 'sell') {
-      group.sellTotal++;
-      group.sellSize += trade.size;
-    }
-    group.netSize = group.buySize - group.sellSize;
-    group.endPrice = trade.price;
-    group.trades.push(trade);
-  } else {
-    group = {
-      id: getDateTimeId(trade.time),
-      message: '',
-      startPrice: trade.price,
-      endPrice: trade.price,
-      netSize: 0,
-      zScore: 0,
-      buyTotal: 0,
-      buySize: 0,
-      sellTotal: 0,
-      sellSize: 0,
-      trades: [trade]
+function normalizeData(items) {
+  return items.map(item => {
+    return {
+      time: item.time,
+      side: item.side,
+      price: Number(item.price),
+      size: Number(item.size),
+      amount: Number(item.price) * Number(item.size)
     };
-    if (trade.side === 'buy') {
-      group.buyTotal++;
-      group.buySize += trade.size;
-    } else if (trade.side === 'sell') {
-      group.sellTotal++;
-      group.sellSize += trade.size;
-    }
-    group.netSize = group.buySize - group.sellSize;
-    groups.push(group);
+  });
+}
+
+function groupBySecond(items) {
+  function getDateTimeId(time) {
+    const datetime = new Date(time);
+    const year = datetime.getUTCFullYear();
+    const month = datetime.getUTCMonth() + 1;
+    const date = datetime.getUTCDate();
+    const hour = datetime.getUTCHours();
+    const minute = datetime.getUTCMinutes();
+    const second = datetime.getUTCSeconds();
+    return `${year}-${month}-${date}T${hour}:${minute}:${second}Z`;
   }
-  return groups;
-}
-
-function getDateTimeId(time) {
-  const datetime = new Date(time);
-  const year = datetime.getUTCFullYear();
-  const month = datetime.getUTCMonth() + 1;
-  const date = datetime.getUTCDate();
-  const hour = datetime.getUTCHours();
-  const minute = datetime.getUTCMinutes();
-  const second = datetime.getUTCSeconds();
-  return `${year}-${month}-${date}T${hour}:${minute}:${second}Z`;
-}
-
-const netSizes = result.map(r => r.netSize);
-// console.log('netSizes', netSizes);
-const q1 = ss.quantile(netSizes, 0.25);
-console.log('Q1', q1);
-const q3 = ss.quantile(netSizes, 0.75);
-console.log('Q3', q3);
-const iqr = ss.interquartileRange(netSizes);
-console.log('IQR', iqr);
-const mean = ss.mean(netSizes);
-console.log('mean', mean);
-const mode = ss.mode(netSizes);
-console.log('mode', mode);
-const median = ss.median(netSizes);
-console.log('median', median);
-const std = ss.standardDeviation(netSizes);
-console.log('Standard Deviation', std);
-
-const K1 = 1.5;
-const K2 = 3;
-let lower2 = q1 - K2 * iqr;
-let lower1 = q1 - K1 * iqr;
-let upper1 = q3 + K1 * iqr;
-let upper2 = q3 + K2 * iqr;
-console.log('Tukey Fences', lower2, lower1, upper1, upper2);
-
-function applyIQR1(result) {
-  result.forEach(trade => {
-    if (trade.netSize < lower1) {
-      trade.message += 'SELL';
-    } else if (trade.netSize > upper1) {
-      trade.message += 'BUY';
+  return items.reduce((groups, trade) => {
+    const id = getDateTimeId(trade.time);
+    let group = groups.find(g => g.id === id);
+    if (group) {
+      if (trade.side === 'buy') {
+        group.buyTotal++;
+        group.buySize += trade.size;
+      } else if (trade.side === 'sell') {
+        group.sellTotal++;
+        group.sellSize += trade.size;
+      }
+      group.netSize = group.buySize - group.sellSize;
+      group.endPrice = trade.price;
+      group.trades.push(trade);
+    } else {
+      group = {
+        id: id,
+        message: '',
+        startPrice: trade.price,
+        endPrice: trade.price,
+        netSize: 0,
+        zScore: 0,
+        buyTotal: 0,
+        buySize: 0,
+        sellTotal: 0,
+        sellSize: 0,
+        time: trade.time,
+        trades: [trade]
+      };
+      if (trade.side === 'buy') {
+        group.buyTotal++;
+        group.buySize += trade.size;
+      } else if (trade.side === 'sell') {
+        group.sellTotal++;
+        group.sellSize += trade.size;
+      }
+      group.netSize = group.buySize - group.sellSize;
+      groups.push(group);
     }
-  });
+    return groups;
+  }, []);
 }
 
-function applyIQR2(result) {
-  result.forEach(trade => {
-    if (trade.netSize < lower2) {
-      trade.message += 'SELL';
-    } else if (trade.netSize > upper2) {
-      trade.message += 'BUY';
+function applyIQR(items, filter) {
+  const netSizes = items.map(item => item.netSize);
+  // console.log('netSizes', netSizes);
+  const q1 = ss.quantile(netSizes, 0.25);
+  console.log('Q1', q1);
+  const q3 = ss.quantile(netSizes, 0.75);
+  console.log('Q3', q3);
+  const iqr = ss.interquartileRange(netSizes);
+  console.log('IQR', iqr);
+  const lower = q1 - K * iqr;
+  const upper = q3 + K * iqr;
+  console.log(`Tukey Fences [${lower}, ${upper}]`);
+  let result = items.map(trade => {
+    let message;
+    if (trade.netSize < lower) {
+      message = 'SELL';
+    } else if (trade.netSize > upper) {
+      message = 'BUY';
     }
+    return {
+      ...trade,
+      message: message
+    };
   });
-}
-
-function applyIQR(result) {
-  result.forEach(trade => {
-    if (trade.netSize < lower2) {
-      trade.message += 'SELL SELL';
-    } else if (trade.netSize >= lower2 && trade.netSize <= lower1) {
-      trade.message += 'SELL';
-    } else if (trade.netSize >= upper1 && trade.netSize <= upper2) {
-      trade.message += 'BUY';
-    } else if (trade.netSize > upper2) {
-      trade.message += 'BUY BUY';
-    }
-  });
-}
-
-function applyZScore(result, zScore) {
-  if (trade.zScore > zScore) {
-    trade.message += 'BUY';
-  } else if (trade.zScore < -zScore) {
-    trade.message += 'SELL';
+  if (filter) {
+    return result.filter(t => !!t.message);
+  } else {
+    return result;
   }
 }
 
-applyIQR(result);
+function applyZScore(items) {
+  const netSizes = result.map(r => r.netSize);
+  // console.log('netSizes', netSizes);
+  const mean = ss.mean(netSizes);
+  console.log('Mean', mean);
+  const mode = ss.mode(netSizes);
+  console.log('Mode', mode);
+  const median = ss.median(netSizes);
+  console.log('Median', median);
+  const std = ss.standardDeviation(netSizes);
+  console.log('Standard Deviation', std);
+  return items.map(trade => {
+    return {
+      ...trade,
+      zScore: ss.zScore(trade.netSize, mean, std)
+    };
+  });
+}
+
+let result = [];
+result = normalizeData(data);
+result = groupBySecond(result);
+result = applyIQR(result, true);
+//result = applyZScore(result);
 
 function getQueueSum(size) {
   if (queue.length >= QUEUE_SIZE) {
