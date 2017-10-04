da806540f13e908db6926186e4e74b0711ce58c6
Tommy Jiang <tianyangj@gmail.com>
2017-10-04 00:54:56 -0700

    orderbook

--- examples/gdax-orderbook.ts
+++ examples/gdax-orderbook.ts
@@ -6,59 +6,51 @@ import { CumulativePriceLevel } from "../src/lib";
 
 const product = 'ETH-USD';
 const logger = GTT.utils.ConsoleLoggerFactory();
-const printOrderbook = GTT.utils.printOrderbook;
-const printTicker = GTT.utils.printTicker;
-/*
- Simple demo that sets up a live order book and then periodically prints some stats to the console.
- */
 
-let tradeVolume: number = 0;
+const MongoClient = require('mongodb').MongoClient;
 
-GTT.Factories.GDAX.FeedFactory(logger, [product]).then((feed: GDAXFeed) => {
-    // Configure the live book object
-    const config: LiveBookConfig = {
-        product: product,
-        logger: logger
-    };
-    const book = new LiveOrderbook(config);
-    book.on('LiveOrderbook.snapshot', () => {
-        logger.log('info', 'Snapshot received by LiveOrderbook Demo');
-        console.log(printOrderbook(book));
-        // setInterval(() => {
-        //     console.log(printOrderbook(book));
-        //     printOrderbookStats(book);
-        //     logger.log('info', `Cumulative trade volume: ${tradeVolume.toFixed(4)}`);
-        // }, 5000);
-    });
-    book.on('LiveOrderbook.ticker', (ticker: Ticker) => {
-        console.log('got ticker', ticker)
-        //console.log(printTicker(ticker));
-    });
-    book.on('LiveOrderbook.trade', (trade: TradeMessage) => {
-        console.log('got trade', trade)
-        tradeVolume += +(trade.size);
-    });
-    book.on('LiveOrderbook.skippedMessage', (details: SkippedMessageEvent) => {
-        // On GDAX, this event should never be emitted, but we put it here for completeness
-        console.log('SKIPPED MESSAGE', details);
-        console.log('Reconnecting to feed');
-        feed.reconnect(0);
-    });
-    book.on('end', () => {
-        console.log('Orderbook closed');
-    });
-    book.on('error', (err) => {
-        console.log('Livebook errored: ', err);
+const uri = 'mongodb://tianyangj:qwer1234@gdax-shard-00-00-xfxjs.mongodb.net:27017,gdax-shard-00-01-xfxjs.mongodb.net:27017,gdax-shard-00-02-xfxjs.mongodb.net:27017/gdax?ssl=true&replicaSet=gdax-shard-0&authSource=admin';
+
+MongoClient.connect(uri, (err, db) => {
+
+    if (err) {
+        console.log('MongoDB failed');
+        db.close();
+    }
+
+    const orderbookCollection = db.collection('orderbook');
+
+    GTT.Factories.GDAX.FeedFactory(logger, [product]).then((feed: GDAXFeed) => {
+        // Configure the live book object
+        const config: LiveBookConfig = {
+            product: product,
+            logger: logger
+        };
+        const book = new LiveOrderbook(config);
+
+        book.on('LiveOrderbook.trade', (trade: TradeMessage) => {
+            const state = book.state();
+            let temp = {
+                ...trade,
+                orderbook: {
+                    bids: [],
+                    asks: []
+                }
+            };
+            temp.orderbook.bids = state.bids.slice(0, 20).map(bid => {
+                return {
+                    price: bid.price.toString(),
+                    size: bid.totalSize.toString()
+                };
+            });
+            temp.orderbook.asks = state.asks.slice(0, 20).map(ask => {
+                return {
+                    price: ask.price.toString(),
+                    size: ask.totalSize.toString()
+                };
+            });
+            orderbookCollection.insertMany([temp], () => { });
+        });
         feed.pipe(book);
     });
-    feed.pipe(book);
 });
-
-function printOrderbookStats(book: LiveOrderbook) {
-    console.log(`Number of bids:       \t${book.numBids}\tasks: ${book.numAsks}`);
-    console.log(`Total ${book.baseCurrency} liquidity: \t${book.bidsTotal.toFixed(3)}\tasks: ${book.asksTotal.toFixed(3)}`);
-    let orders: CumulativePriceLevel[] = book.ordersForValue('buy', 100, false);
-    console.log(`Cost of buying 100 ${book.baseCurrency}: ${orders[orders.length - 1].cumValue.toFixed(2)} ${book.quoteCurrency}`);
-    orders = book.ordersForValue('sell', 1000, true);
-    console.log(`Need to sell ${orders[orders.length - 1].cumSize.toFixed(3)} ${book.baseCurrency} to get 1000 ${book.quoteCurrency}`);
-}
